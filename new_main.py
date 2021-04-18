b361d9070d5172e07b749327378d214da1182876
Holo4ka <60144686+Holo4ka@users.noreply.github.com>
2021-04-18 11:34:12 +0300

    skip, stop, pause, etc. commands

--- new_main.py
+++ new_main.py
@@ -2,8 +2,9 @@ import discord
 import youtube_dl
 from discord.ext import commands
 import asyncio
+import requests
 
-TOKEN = 'ODI0MTY2OTg3NTQxNjQzMjk0.YFrbUg.yLNjZ8Ia4vFMd3gdaW_hj5rFo4U'
+
 bot = commands.Bot(command_prefix='~')
 
 ytdl_format_options = {
@@ -27,29 +28,19 @@ ffmpeg_options = {
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 players = {}
+queue = asyncio.Queue()
+player = None
 
 
-# Класс для превращения ссылки в объект, который может проиграть discord-бот
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
+class UrlError(Exception):
+    pass
 
-        self.data = data
 
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # Взять первый объект
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+def is_url(string: str):
+    protocol = string.startswith('https://') or string.startswith('http://')
+    address = 'youtube.com' in string
+    params = 'watch?v=' in string
+    return protocol and address and params
 
 
 @bot.event
@@ -63,25 +54,72 @@ async def on_ready():
 
 
 @bot.command(name='play', pass_context=True)
-async def play(ctx, url):
-    author_channel = ctx.message.author.voice.channel
-    await author_channel.connect()
-    # # server = ctx.message.server
-    # player = author_channel.create_ytdl_player(url)
-    # # players[server.id] = player
-    # player.start()
-    with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
-        song_info = ydl.extract_info(url, download=False)
-    ctx.message.guild.voice_client.play(
-        discord.FFmpegPCMAudio(song_info["formats"][0]["url"], executable='ffmpeg/bin/ffmpeg.exe'))
-    ctx.message.guild.voice_client.source = discord.PCMVolumeTransformer(ctx.message.guild.voice_client.source)
-    ctx.message.guild.voice_client.source.volume = 1
+async def play(ctx, *url):
+    try:
+        global player
+        if len(url) == 0:
+            return
+        elif len(url) != 1 or not is_url(url[0]):
+            url = ' '.join(url)
+            if 'youtube.com' not in url:
+                raise UrlError
+            parts = '%20'.join(url.split())
+            res = requests.get(
+                f'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={parts}&key={YOUTUBE_TOKEN}').json()
+            id = res['items'][0]['id']['videoId']
+            url = f'https://www.youtube.com/watch?v={id}'
+        if player is None:
+            author_channel = ctx.message.author.voice.channel
+            await author_channel.connect()
+            player = ctx.message.guild.voice_client
+        await queue.put(url)
+        await ctx.message.channel.send('Добавлено в очередь:\n' + url)
+        while not queue.empty():
+            if not player.is_playing():
+                url = await queue.get()
+                with youtube_dl.YoutubeDL(ytdl_format_options) as ydl:
+                    song_info = ydl.extract_info(url, download=False)
+                ctx.message.guild.voice_client.play(
+                    discord.FFmpegPCMAudio(song_info["formats"][0]["url"], executable='ffmpeg.exe'))
+                ctx.message.guild.voice_client.source = discord.PCMVolumeTransformer(ctx.message.guild.voice_client.source)
+                ctx.message.guild.voice_client.source.volume = 1
+                await ctx.message.channel.send('Сейчас играет:\n' + url)
+            elif player.is_playing():
+                await asyncio.sleep(1)
+    except UrlError:
+        await ctx.message.channel.send('Некорректная ссылка')
+    except Exception as e:
+        print(e)
 
 
-@bot.command(pass_context=True, name='leave')
-async def leave(ctx):
-    author_channel = ctx.message.author.voice.voice_channel
-    await author_channel.disconnect()
+@bot.command(pass_context=True, name='pause')
+async def pause(ctx):
+    if ctx.message.guild.voice_client.is_playing():
+        ctx.message.guild.voice_client.pause()
+
+
+@bot.command(pass_context=True, name='stop')
+async def stop(ctx):
+    global queue, player
+    ctx.message.guild.voice_client.stop()
+    await ctx.message.guild.voice_client.disconnect()
+    queue = asyncio.Queue()
+    await ctx.message.channel.send('Проигрывание завершено')
+    player = None
+
+
+@bot.command(pass_context=True, name='resume')
+async def resume(ctx):
+    if ctx.message.guild.voice_client.is_paused():
+        ctx.message.guild.voice_client.resume()
+
+
+@bot.command(pass_context=True, name='skip')
+async def skip(ctx):
+    global queue, player
+    ctx.message.guild.voice_client.stop()
+    if not queue.empty():
+        await ctx.message.channel.send('Трек пропущен')
 
 
 bot.run(TOKEN)
